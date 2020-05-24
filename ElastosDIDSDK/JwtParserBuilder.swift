16f2e72552caaa8498171736e820a2900106cbc7
liaihong <478701610@qq.com>
2020-05-24 18:15:50 +0800

    implement JwtParserBuilder

--- ElastosDIDSDK/JwtParserBuilder.swift
+++ ElastosDIDSDK/JwtParserBuilder.swift
@@ -3,7 +3,31 @@ import UIKit
 
 class JwtParserBuilder: NSObject {
 
+    var claimsJwt : String?
+
     public func parseClaimsJwt(_ claimsJwt: String) throws -> JWT {
         return try JWT(jwtString: claimsJwt)
+    }
+
+    public func build() -> JwtParser {
+        return JwtParser()
+    }
+}
+
+public class JwtParser: NSObject {
+
+    var jwt: JWT?
+
+    public func parseClaimsJwt(_ claimsJwt: String) throws -> JWT {
+        return try JWT(jwtString: claimsJwt)
+    }
+
+
+    public func getHeader() throws -> Header {
+        return jwt!.header;
+    }
+
+    public func getBody() throws -> Claims {
+        return jwt!.claims;
     }
 }
