30d8099e8b60c533025dc2509cb4b5bf3e4cdb7c
liaihong <>
2021-07-30 09:57:46 +0800

    CU-8nxu8k  adapter macOS
    (cherry picked from commit 13843fd2a1ff1ed3620fe2cdedfe06abce52cee1)

---
+++ ElastosDIDSDK _macOS/ElastosDIDSDK__macOS.h
@@ -0,0 +1,19 @@
+//
+//  ElastosDIDSDK__macOS.h
+//  ElastosDIDSDK _macOS
+//
+//  Created by 李爱红 on 2021/7/30.
+//  Copyright © 2021 elastos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ElastosDIDSDK__macOS.
+FOUNDATION_EXPORT double ElastosDIDSDK__macOSVersionNumber;
+
+//! Project version string for ElastosDIDSDK__macOS.
+FOUNDATION_EXPORT const unsigned char ElastosDIDSDK__macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElastosDIDSDK__macOS/PublicHeader.h>
+
+
---
+++ ElastosDIDSDK_macOS/ElastosDIDSDK_macOS.h
@@ -0,0 +1,19 @@
+//
+//  ElastosDIDSDK_macOS.h
+//  ElastosDIDSDK_macOS
+//
+//  Created by 李爱红 on 2021/7/30.
+//  Copyright © 2021 elastos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ElastosDIDSDK_macOS.
+FOUNDATION_EXPORT double ElastosDIDSDK_macOSVersionNumber;
+
+//! Project version string for ElastosDIDSDK_macOS.
+FOUNDATION_EXPORT const unsigned char ElastosDIDSDK_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElastosDIDSDK_macOS/PublicHeader.h>
+
+
