cb46c582d92b5f87020b61f498d70347e9474d05
liaihong <>
2021-08-05 11:00:58 +0800

    CU-8nxu8k fix setting
    (cherry picked from commit d436db18c7182257a2730689f896bd92e2cf8a7c)

--- ElastosDIDSDK _macOS/ElastosDIDSDK__macOS.h
+++ ElastosDIDSDK _macOS/ElastosDIDSDK__macOS.h
@@ -1,10 +1,24 @@
-//
-//  ElastosDIDSDK__macOS.h
-//  ElastosDIDSDK _macOS
-//
-//  Created by 李爱红 on 2021/7/30.
-//  Copyright © 2021 elastos. All rights reserved.
-//
+/*
+ * Copyright (c) 2021 Elastos Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT INOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #import <Foundation/Foundation.h>
 
@@ -16,4 +30,5 @@ FOUNDATION_EXPORT const unsigned char ElastosDIDSDK__macOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ElastosDIDSDK__macOS/PublicHeader.h>
 
-
+#include "crypto.h"
+#include "HDkey.h"
