feaa3cf6be7e69c9ea43fe4bacbf8ab3b6c8258f
liaihong <>
2021-11-19 14:54:52 +0800

    Update contract address

--- ElastosDIDSDKTests/TestConfig.swift
+++ ElastosDIDSDKTests/TestConfig.swift
@@ -13,13 +13,13 @@ let walletPath = "/Users/liaihong/new-idchain.privnet.wallet"
 //let rpcEndpoint = "http://52.80.107.251:1111"
 //let rpcEndpoint = "https://api-testnet.elastos.io/newid"
 //let rpcEndpoint = "https://api.trinity-tech.cn/eid"
-let rpcEndpoint = "https://api-testnet.trinity-tech.cn/eid"
+let rpcEndpoint = "https://api-testnet.trinity-tech.cn/eid"// 目前测试使用
 //                  "https://api-testnet.elastos.io/eid"
 
 //let contractAddress = "0xEA2256bd30cfeC643203d1a6f36A90A4fD17863E"
 //let contractAddress = "0x358370716A6BE0f6EB1178C6bd08676c279a3A9b"
-let contractAddress = "0xdDCF19F9A52BC3c58F89C43BfB3614293F977ccA"
-//let contractAddress = "0xF654c3cBBB60D7F4ac7cDA325d51E62f47ACD436" // 测试错误的did
+//let contractAddress = "0xdDCF19F9A52BC3c58F89C43BfB3614293F977ccA"
+let contractAddress = "0xF654c3cBBB60D7F4ac7cDA325d51E62f47ACD436" // 目前测试使用
 let walletPassword = "password"
 
 
