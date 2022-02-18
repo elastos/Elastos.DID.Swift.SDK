9e26cc60c12429b607279a8df2e8cc8fe7a4904c
liaihong <>
2022-02-18 09:39:09 +0800

    CU-29wxf5p Modify the error of failing to compile the document

--- docs.sh
+++ docs.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-FIRST_ARGUMENT="$1"
-if [[ $FIRST_ARGUMENT == "objc" ]]
-then
-appledoc --project-name ElastosDIDSDK --project-company "elastos.org" --company-id org.elastos --no-create-docset --output ./oc-docs/ .
-else
-jazzy -x -workspace,ElastosDIDSDK.xcworkspace,-scheme,ElastosDIDSDK
-fi
+jazzy --podspec ElastosDIDSDK.podspec
